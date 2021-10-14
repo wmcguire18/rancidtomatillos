a01b323f49c81f077178c10e3eca71b8939c97f4
Paul Chambers <paultimothychambers@gmail.com>
2021-10-14 15:32:51 -0600

    Apply needed import statements and whittle down code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import movieData from './MovieData.js';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="nav-bar">
+        <header className="nav-bar__header">
+          <h1 className="header__app-name"></h1>
+          <img className="header__logo" src=""/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
