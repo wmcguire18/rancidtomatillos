981607b3e9d40d5e78bb1079f76c5425ff95be5e
Paul Chambers <paultimothychambers@gmail.com>
2021-10-16 23:13:51 -0600

    Refactor to fetch data through api calls

--- src/MoviesContainer.js
+++ src/MoviesContainer.js
@@ -3,42 +3,54 @@ import MovieCard from './MovieCard.js';
 import SingleMovie from './SingleMovie.js';
 import './MoviesContainer.css';
 
-const MoviesContainer = (movieData) => {
-console.log(movieData.isShowingDetails);
-console.log(movieData.selectedMovie);
-  const movieCards = movieData.movies.map(movie => {
+const MoviesContainer = (props) => {
+  const movieCards = props.movies.map(movie => {
     return (
       <MovieCard
         key={ movie.id }
         id={ movie.id }
         title={ movie.title }
         src={ movie.poster_path }
-        handleClick={ movieData.handleClick }
+        handleClick={ props.handleClick }
       />
     )
   })
 
-  return (
-    <section>
-    { !movieData.isShowingDetails &&
-      <div className="movies-container__featured">
-        <h2 className="movies-container__featured-text">FEATURED:</h2>
-        <article className="featured__card-holder">
-          {movieCards}
-        </article>
-      </div>
-    }
-    { movieData.isShowingDetails &&
-      <SingleMovie
-      key={ movieData.selectedMovie.id }
-      title={ movieData.selectedMovie.title}
-      src={ movieData.selectedMovie.backdrop_path}
-      handleClick={ movieData.handleClick }
-      />
+  if (!props.isShowingDetails) {
+    return (
+      <section>
+        <div className="movies-container__featured">
+          <h2 className="movies-container__featured-text">FEATURED:</h2>
+          <article className="featured__card-holder">
+            {movieCards}
+          </article>
+        </div>
+      </section>
+    )
 
-    }
-    </section>
-  )
+  } else if (props.isShowingDetails) {
+    console.log(props.selectedMovie)
+    return (
+      <section>
+        <SingleMovie
+          key={ props.selectedMovie.id }
+          title={ props.selectedMovie.title }
+          average_rating={ props.selectedMovie.average_rating }
+          release_date={ props.selectedMovie.release_date }
+          genres={ props.selectedMovie.genres }
+          overview={ props.selectedMovie.overview }
+          tagline={ props.selectedMovie.tagline }
+          budget={ props.selectedMovie.budget }
+          src={ props.selectedMovie.backdrop_path }
+          handleClick={ props.handleClick }
+        />
+      </section>
+    )
+  } else {
+    return (
+      <p>...loading...</p>
+    )
+  }
 }
 
 export default MoviesContainer;
