1a1ad8aefc6fedec0a8701a4783645944ad4c455
Paul Chambers <paultimothychambers@gmail.com>
2021-10-16 23:20:19 -0600

    Refactor

--- src/MoviesContainer.js
+++ src/MoviesContainer.js
@@ -4,6 +4,7 @@ import SingleMovie from './SingleMovie.js';
 import './MoviesContainer.css';
 
 const MoviesContainer = (props) => {
+  console.log(props.selectedMovie)
   const movieCards = props.movies.map(movie => {
     return (
       <MovieCard
@@ -16,41 +17,34 @@ const MoviesContainer = (props) => {
     )
   })
 
-  if (!props.isShowingDetails) {
-    return (
-      <section>
-        <div className="movies-container__featured">
-          <h2 className="movies-container__featured-text">FEATURED:</h2>
-          <article className="featured__card-holder">
-            {movieCards}
-          </article>
-        </div>
-      </section>
-    )
-
-  } else if (props.isShowingDetails) {
-    console.log(props.selectedMovie)
-    return (
-      <section>
-        <SingleMovie
-          key={ props.selectedMovie.id }
-          title={ props.selectedMovie.title }
-          average_rating={ props.selectedMovie.average_rating }
-          release_date={ props.selectedMovie.release_date }
-          genres={ props.selectedMovie.genres }
-          overview={ props.selectedMovie.overview }
-          tagline={ props.selectedMovie.tagline }
-          budget={ props.selectedMovie.budget }
-          src={ props.selectedMovie.backdrop_path }
-          handleClick={ props.handleClick }
-        />
-      </section>
-    )
-  } else {
-    return (
-      <p>...loading...</p>
-    )
-  }
+  return (
+    <section>
+      {
+        !props.isShowingDetails &&
+          <div className="movies-container__featured">
+            <h2 className="movies-container__featured-text">FEATURED:</h2>
+            <article className="featured__card-holder">
+              {movieCards}
+            </article>
+          </div>
+      }
+      {
+        props.isShowingDetails &&
+          <SingleMovie
+            key={ props.selectedMovie.id }
+            title={ props.selectedMovie.title }
+            average_rating={ props.selectedMovie.average_rating }
+            release_date={ props.selectedMovie.release_date }
+            genres={ props.selectedMovie.genres }
+            overview={ props.selectedMovie.overview }
+            tagline={ props.selectedMovie.tagline }
+            budget={ props.selectedMovie.budget }
+            src={ props.selectedMovie.backdrop_path }
+            handleClick={ props.handleClick }
+          />
+      }
+    </section>
+  )
 }
 
 export default MoviesContainer;
