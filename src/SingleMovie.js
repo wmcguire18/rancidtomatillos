7176c1ff7fc030ab2a25e4c32455158406b01b8b
Paul Chambers <paultimothychambers@gmail.com>
2021-10-19 18:42:33 -0600

    Refactor for router

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SingleMovie.css';
 
-const SingleMovie = ({title, average_rating, release_date, src, handleClick, genres, overview, tagline, budget}) => {
+const SingleMovie = ({title, average_rating, release_date, backdrop_path, genres, overview, tagline, budget}) => {
   return (
     <div className="single-movie-info">
       <img
         className="single-movie-info__movie-poster"
-        src={src}
+        src={backdrop_path}
         alt="Large backdrop image of a promotional movie poster"
       />
       <h2 className="single-movie-info__title">{title}</h2>
-      <p className="single-movie-view__rating">{ average_rating }</p>
+      <p className="single-movie-view__rating">{average_rating}</p>
       <p className="single-movie-view__release-date">{release_date}</p>
       <p className="single-movie-view__genres">{genres}</p>
       <p className="single-movie-view__overview">{overview}</p>
       <p className="single-movie-view__tagline">{tagline}</p>
       <p className="single-movie-view__budget">{budget}</p>
-      <button className="single-movie-info__home-view-button" onClick={handleClick}>Return to Home</button>
+      <button className="single-movie-info__home-view-button">Return to Home</button>
     </div>
   )
 }
