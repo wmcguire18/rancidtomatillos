39bc06dbf9a2578d71c3e640143fc6ce107934ff
Paul Chambers <paultimothychambers@gmail.com>
2021-10-20 02:01:42 -0600

    Refactor for router

--- src/MoviesContainer.js
+++ src/MoviesContainer.js
@@ -2,46 +2,30 @@ import React from 'react';
 import MovieCard from './MovieCard.js';
 import SingleMovie from './SingleMovie.js';
 import './MoviesContainer.css';
+import img1 from './theater2.jpeg';
 
-const MoviesContainer = (props) => {
-  const movieCards = props.movies.map(movie => {
+
+
+const MoviesContainer = ({movies}) => {
+  const movieCards = movies.map(movie => {
     return (
       <MovieCard
         key={ movie.id }
         id={ movie.id }
         title={ movie.title }
         src={ movie.poster_path }
-        handleClick={ props.handleClick }
       />
     )
   })
 
   return (
     <section>
-      {
-        !props.isShowingDetails &&
-          <div className="movies-container__featured">
-            <h2 className="movies-container__featured-text">FEATURED:</h2>
-            <article className="featured__card-holder">
-              {movieCards}
-            </article>
-          </div>
-      }
-      {
-        props.isShowingDetails &&
-          <SingleMovie
-            key={ props.selectedMovie.id }
-            title={ props.selectedMovie.title }
-            average_rating={ props.selectedMovie.average_rating }
-            release_date={ props.selectedMovie.release_date }
-            genres={ props.selectedMovie.genres }
-            overview={ props.selectedMovie.overview }
-            tagline={ props.selectedMovie.tagline }
-            budget={ props.selectedMovie.budget }
-            src={ props.selectedMovie.backdrop_path }
-            handleClick={ props.handleClick }
-          />
-      }
+    <img className="background-image" src={img1}/>
+      <div className="movies-container__featured">
+        <article className="featured__card-holder">
+          {movieCards}
+        </article>
+      </div>
     </section>
   )
 }
