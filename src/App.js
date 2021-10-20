09dd126442df4807d6500f84b24cf37e5febcefb
Paul Chambers <paultimothychambers@gmail.com>
2021-10-20 02:00:52 -0600

    Refactor for router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
-import Loading from './Loading.js';
 import SingleMovie from './SingleMovie.js';
 import MoviesContainer from './MoviesContainer.js';
 import {
@@ -12,36 +11,26 @@ import {
   // submitNewUserRating
 } from './apiCalls.js';
 import './App.css';
+import img1 from './nav-bkg2.png';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       movies: [],
-      singleMovies: [],
-      error: null,
-      movieToRender: {}
+      error: null
     }
   }
 
   componentDidMount = () => {
     loadMovies().then(data => this.setState({ movies: data.movies }))
-    .then(() => this.getSingleMovieInfo(this.state.movies))
-    .catch(error => this.setState({ error: error.message }))
-  }
-
-  getSingleMovieInfo = (movies) => {
-    movies.map(movie => {
-      loadSingleMovie(movie.id).then(data => {
-        this.state.singleMovies.push(data.movie)
-      })
-    })
     .catch(error => this.setState({ error: error.message }))
   }
 
   render() {
     return (
       <main>
+        <img className="nav-bkg" src={img1} />
         <nav className="nav-bar">
           <h1 className="nav-bar__app-name">Mile-High Movies</h1>
           <img className="nav-bar__logo" src="" />
@@ -57,10 +46,10 @@ class App extends Component {
         <Route
           exact path="/movie_info/:id"
           render={({match}) => {
-            this.state.movieToRender = this.state.singleMovies.find(movie => {
+            const movieToRender = this.state.movies.find(movie => {
               return movie.id === parseInt(match.params.id)
             })
-            return <SingleMovie {...this.state.movieToRender} />
+            return <SingleMovie id={ movieToRender }/>
           }}
         />
       </main>
