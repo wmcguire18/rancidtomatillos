4d30f7681b9cbcd3e25aecf3c322aa4f69276340
William McGuire <wmcguire18@gmail.com>
2021-10-23 14:47:59 -0400

    Single movie styling

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -45,6 +45,8 @@ class SingleMovie extends Component {
         <Link to="/">
           <button className="single-movie-info__home-view-button">Return to Home</button>
         </Link>
+          <button className="single-movie-info__watched-movie-button">I've Seen It!</button>
+          <button className="single-movie-info__queued-movie-button">I Want To See It!</button>
         <div className="single-movie-info__info-background">
         </div>
       </section>
