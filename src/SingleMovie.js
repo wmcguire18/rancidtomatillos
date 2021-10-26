4699ea0eb78352e51175e70e0c362b1f4cf982e6
William McGuire <wmcguire18@gmail.com>
2021-10-26 04:50:38 -0400

    Removed dead button

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -84,8 +84,6 @@ class SingleMovie extends Component {
         <Link to="/">
           <button className="single-movie-info__home-view-button">Return to Home</button>
         </Link>
-
-          <button className="single-movie-info__watched-movie-button">I've Seen It!</button>
           <button className="single-movie-info__queued-movie-button" onClick={() => this.props.queueMovie(this.state.movieId.id)}>I Want To See It!</button>
 
         <div className="iframe-container">
